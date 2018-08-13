5b92691d6f43c2735dd9148af4de1f03a6c65aef
Robby Gunawan Sutanto <rgunawans@hotmail.com>
2018-08-13 17:30:54 +0700

    add msgbox
    add msgbox, a custom messagebox

---
+++ src/miscForms/msgBox.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace miscForms
+{
+    public partial class msgBox : Form
+    {
+        public string _ErrorMessage;
+        public string _photoURL;
+        public string _warna;
+        public string _returnform;
+
+        public msgBox()
+        {
+            InitializeComponent();
+            //panjang max: 750, hidden picturebox
+            //panjang min: 497, show picturebox
+        }
+
+        private void buttonAdv1_Click(object sender, EventArgs e)
+        {
+            if (_returnform != "")
+            {
+                this.Dispose();
+                
+                var form = Activator.CreateInstance(Type.GetType("namespace." + _returnform)) as Form;
+
+                form.ShowDialog();
+
+            }
+            else
+            {
+                this.Dispose();
+            }
+        }
+
+        private void frmMessageBox_Load(object sender, EventArgs e)
+        {
+            if (_warna == "Merah")
+            {
+                this.BackColor = Color.DarkRed;
+                btnClose.BackColor = Color.DarkRed;
+            }
+            else
+            {
+                this.BackColor = Color.DarkBlue;
+                btnClose.BackColor = Color.DarkBlue;
+            }
+            if (_photoURL == "")
+            {
+                lblError.Width = 750;
+                pBoxCapture.Visible = false;
+                
+            }
+            else
+            {
+                lblError.Width = 497;
+                pBoxCapture.Visible = true;
+                pBoxCapture.Image = new Bitmap(_photoURL);
+
+            }
+            lblError.Text = _ErrorMessage;
+        }
+    }
+}
---
+++ src/miscForms/msgBox.designer.cs
@@ -0,0 +1,98 @@
+﻿ namespace CoronaQueue
+
+{
+    partial class frmMessageBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblError = new System.Windows.Forms.Label();
+            this.pBoxCapture = new System.Windows.Forms.PictureBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxCapture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblError
+            // 
+            this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblError.ForeColor = System.Drawing.Color.White;
+            this.lblError.Location = new System.Drawing.Point(13, 31);
+            this.lblError.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(663, 235);
+            this.lblError.TabIndex = 1;
+            this.lblError.Text = "<< Error Message >>";
+            this.lblError.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pBoxCapture
+            // 
+            this.pBoxCapture.Location = new System.Drawing.Point(713, 31);
+            this.pBoxCapture.Margin = new System.Windows.Forms.Padding(4);
+            this.pBoxCapture.Name = "pBoxCapture";
+            this.pBoxCapture.Size = new System.Drawing.Size(329, 235);
+            this.pBoxCapture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxCapture.TabIndex = 2;
+            this.pBoxCapture.TabStop = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.DarkBlue;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(404, 287);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(137, 63);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.buttonAdv1_Click);
+            // 
+            // frmMessageBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkBlue;
+            this.ClientSize = new System.Drawing.Size(1059, 362);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.pBoxCapture);
+            this.Controls.Add(this.lblError);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmMessageBox";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.frmMessageBox_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxCapture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.PictureBox pBoxCapture;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
